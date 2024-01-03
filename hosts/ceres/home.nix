66a3406ea152237d13032bcab3fd4f578745974c
Jonathan Wurth <joni.htruw@gmail.com>
2024-01-03 11:41:56 +0100

    Remove trailing slashes

--- hosts/ceres/home.nix
+++ hosts/ceres/home.nix
@@ -1,10 +1,10 @@
 # home-manager configuration file of 'ceres' (replaces ~/.config/nixpkgs/home.nix).
 {inputs, ...}: {
   imports = [
-    ../../home/
+    ../../home
 
     # Use CLI tools.
-    ../../home/cli/
+    ../../home/cli
   ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
