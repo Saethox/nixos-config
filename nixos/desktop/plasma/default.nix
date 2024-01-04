6e55d939d1e72a2be009d9cbde57d980093e8eb7
Jonathan Wurth <joni.htruw@gmail.com>
2024-01-04 19:32:35 +0100

    Add Bismuth to Plasma

--- nixos/desktop/plasma/default.nix
+++ nixos/desktop/plasma/default.nix
@@ -1,10 +1,6 @@
-{
+{pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -14,4 +10,12 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.systemPackages = with pkgs; [
+  	libsForQt5.bismuth # Bismuth window tiling manager extension
+  ];  
 }
