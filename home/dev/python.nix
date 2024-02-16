fec4b7846b913d0fd80b0f921db8137f3c6ec9b0
Jonathan Wurth <joni.htruw@gmail.com>
2024-02-16 12:56:34 +0100

    Add pygments package

--- home/dev/python.nix
+++ home/dev/python.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     # Latest stable Python with useful packages.
-    (python3.withPackages (ps: with ps; [ipython numpy pandas matplotlib seaborn]))
+    (python3.withPackages (ps: with ps; [ipython numpy pandas matplotlib seaborn pygments]))
     poetry # Dependency management
     (builtins.getFlake "github:GuillaumeDesforges/fix-python/c9c8e8a439e0c985d0c5cea757d806837cf322f1") # Path .venv with `fix-python`
   ];
