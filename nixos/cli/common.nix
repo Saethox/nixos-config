9b591f3182cfc0f8b6c6caea94d2d17d18565852
Saethox <joni.htruw@gmail.com>
2024-03-03 19:36:07 +0100

    Add nvidia driver

--- nixos/cli/common.nix
+++ nixos/cli/common.nix
@@ -11,21 +11,25 @@ in {
   config = lib.mkIf cfg.enable {
     modules.cli.fish.enable = lib.mkDefault true;
 
-    environment.systemPackages = with pkgs; [
-      # Fetch files from the internet.
-      wget
-      curl
+    environment.systemPackages =
+      (with pkgs; [
+        # Fetch files from the internet.
+        wget
+        curl
 
-      # ZIP archive tools.
-      zip
-      unzip
+        # ZIP archive tools.
+        zip
+        unzip
 
-      xclip # Clipboard
+        xclip # Clipboard
 
-      neofetch # Display system information
-      micro # Text editor with mouse support
-      just # Command runner
-      htop # System monitor
-    ];
+        neofetch # Display system information
+        micro # Text editor with mouse support
+        just # Command runner
+        htop # System monitor
+      ])
+      ++ (with pkgs.unstable; [
+        lshw # Print hardware configuration
+      ]);
   };
 }
