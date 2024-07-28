c9bba3c85a17c494efaf3168a1edb6f7bfeea4c6
Saethox <joni.htruw@gmail.com>
2024-07-28 15:37:50 +0200

    Disable Nvidia Sync

--- hosts/juno/hardware/default.nix
+++ hosts/juno/hardware/default.nix
@@ -96,7 +96,7 @@
     nvidiaSettings = true;
 
     prime = {
-      sync.enable = true;
+      sync.enable = false;
 
       # Make sure to use the correct Bus ID values for your system!
       # sudo lshw -c display
@@ -105,8 +105,8 @@
 
       # Offload mode.
       offload = {
-        enable = false;
-        enableOffloadCmd = false;
+        enable = true;
+        enableOffloadCmd = true;
       };
     };
 
