b3f5683e51da9891310411cbfdc5d310b235e660
Saethox <joni.htruw@gmail.com>
2024-09-09 14:58:53 +0200

    Add nvidia-offload script

--- hosts/juno/hardware/default.nix
+++ hosts/juno/hardware/default.nix
@@ -122,4 +122,15 @@
       persistencedSha256 = "sha256-gXHBR2+1+YZE2heRArfrZpEF3rO7R92ChuQN+ISpil8=";
     };
   };
+
+  # Script for executing binary with Nvidia GPU.
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '')
+  ];
 }
