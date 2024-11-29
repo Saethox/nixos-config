0199db743250dfc8e77d479d94105033ab2ea7c4
Saethox <joni.htruw@gmail.com>
2024-11-29 15:29:44 +0100

    Update nvidia driver on titan

--- hosts/titan/hardware/default.nix
+++ hosts/titan/hardware/default.nix
@@ -93,6 +93,6 @@
     };
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
