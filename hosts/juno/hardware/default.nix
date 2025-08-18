c9a59a2b4c07156309f1fedebb52fb2501690f82
Saethox <joni.htruw@gmail.com>
2025-08-18 15:55:55 +0200

    Update kernel on juno

--- hosts/juno/hardware/default.nix
+++ hosts/juno/hardware/default.nix
@@ -20,8 +20,7 @@
   hardware.enableAllFirmware = true;
 
   # Use latest kernel.
-  # Seems to break nvidia driver.
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Enable networking.
   networking.networkmanager.enable = true;
@@ -122,7 +121,14 @@
     };
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.65.06";
+      sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+      sha256_aarch64 = "sha256-4CrNwNINSlQapQJr/dsbm0/GvGSuOwT/nLnIknAM+cQ=";
+      openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+      settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+      persistencedSha256 = "sha256-ETRfj2/kPbKYX1NzE0dGr/ulMuzbICIpceXdCRDkAxA=";
+    };
   };
 
   # Script for executing binary with Nvidia GPU.
